3922432eb27fce7c8d476bd540a050c9ea95fa50
JEOZ <jortiz@inteligov.mx>
2022-10-19 13:37:14 -0500

    update java

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -5,9 +5,12 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+echo "oracle-java11-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install openjdk-11-jdk-headless -y
 sudo apt-get install jenkins -y
+
+
+
 
 # Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
